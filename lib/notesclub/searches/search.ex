b23110b5b4259f1eed5b46056a49b2114614cc01
Hector Perez <hecpeare@gmail.com>
2022-08-16 16:28:38 +0200

    searches context

---
+++ lib/notesclub/searches/search.ex
@@ -0,0 +1,25 @@
+defmodule Notesclub.Searches.Search do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "searches" do
+    field :order, :string
+    field :page, :integer
+    field :per_page, :integer
+    field :response_body, :map
+    field :response_headers, :map
+    field :response_notebooks_count, :integer
+    field :response_private, :map
+    field :response_status, :integer
+    field :url, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(search, attrs) do
+    search
+    |> cast(attrs, [:per_page, :page, :order, :url, :response_notebooks_count, :response_status, :response_body, :response_headers, :response_private])
+    |> validate_required([:per_page, :page, :order, :url, :response_notebooks_count, :response_status, :response_body, :response_headers, :response_private])
+  end
+end
