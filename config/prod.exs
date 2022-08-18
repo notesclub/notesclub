6c6566554e081b888404dcdd58a0284be72fc4bc
Hector Perez <hecpeare@gmail.com>
2022-08-18 16:23:59 +0200

    debug log level

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,8 @@ import Config
 config :notesclub, NotesclubWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
