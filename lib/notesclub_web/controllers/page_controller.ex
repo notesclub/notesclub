8b1b3a489432fe9d1e71a2a5c9d8d13618249fad
Hector Perez <hecpeare@gmail.com>
2022-08-18 10:37:58 +0200

    filter by author and repo

--- lib/notesclub_web/controllers/page_controller.ex
+++ lib/notesclub_web/controllers/page_controller.ex
@@ -9,12 +9,24 @@ defmodule NotesclubWeb.PageController do
   def index(conn, _params) do
     notebooks = Notebooks.list_random_notebooks(%{limit: @notebooks_in_home_count})
     notebooks_count = Notebooks.count()
-    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, all: false)
+    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, button: :more, filter: nil)
   end
 
   def all(conn, _params) do
     notebooks = Notebooks.list_notebooks()
     notebooks_count = Notebooks.count()
-    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, all: true)
+    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, button: :less, filter: nil)
+  end
+
+  def author(conn, %{"author" => author}) do
+    notebooks = Notebooks.list_author_notebooks_desc(author)
+    notebooks_count = Notebooks.count()
+    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, button: :more, filter: author)
+  end
+
+  def repo(conn, %{"repo" => repo, "author" => author}) do
+    notebooks = Notebooks.list_repo_author_notebooks_desc(repo, author)
+    notebooks_count = Notebooks.count()
+    render(conn, "index.html", notebooks: notebooks, notebooks_count: notebooks_count, button: :more, filter: "#{author}/#{repo}")
   end
 end
