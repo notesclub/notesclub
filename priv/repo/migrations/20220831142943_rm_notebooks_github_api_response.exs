37c8ccb581c68f12fef9b4594e37e00005de0110
Hector Perez <hecpeare@gmail.com>
2022-08-31 17:14:30 +0200

    make last migration reversible

--- priv/repo/migrations/20220831142943_rm_notebooks_github_api_response.exs
+++ priv/repo/migrations/20220831142943_rm_notebooks_github_api_response.exs
@@ -1,9 +1,15 @@
 defmodule Notesclub.Repo.Migrations.RmNotebooksGithubApiResponse do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table("notebooks") do
       remove :github_api_response
+    end
+  end
+
+  def down do
+    alter table("notebooks") do
+      add :github_api_response, :map
     end
   end
 end
