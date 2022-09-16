be991b07a727658064d336e2c3ce84876a388310
Hassan Raza <hassanrsiddiqi@gmail.com>
2022-09-16 16:38:46 +0500

    remove duplicate migratoins

--- priv/repo/migrations/20220911104619_rename_users_name_with_username 2.exs
+++
@@ -1,9 +0,0 @@
-defmodule Notesclub.Repo.Migrations.RenameReposNameWithUsername do
-  use Ecto.Migration
-
-  def change do
-    drop unique_index(:users, [:name])
-    rename table(:users), :name, to: :username
-    create unique_index(:users, [:username])
-  end
-end
