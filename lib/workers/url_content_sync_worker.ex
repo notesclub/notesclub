950e1448acb5fc343937c8a6daaf6c9fd835f11c
Hector Perez <hecpeare@gmail.com>
2022-10-07 14:58:27 +0200

    rm struct

--- lib/workers/url_content_sync_worker.ex
+++ lib/workers/url_content_sync_worker.ex
@@ -11,15 +11,12 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
     queue: :default,
     unique: [period: 300, states: [:available, :scheduled, :executing]]
 
-  alias Notesclub.Workers.UrlContentSyncWorker, as: Sync
   alias Notesclub.Workers.RepoSyncWorker
   alias Notesclub.Notebooks
   alias Notesclub.Notebooks.Notebook
   alias Notesclub.Notebooks.Urls
   alias Notesclub.Repos.Repo
   alias Notesclub.ReqTools
-
-  defstruct [:notebook, :urls, :default_branch_content, :commit_content, :cancel]
 
   require Logger
 
@@ -30,9 +27,6 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
 
       iex> perform((%Oban.Job{args: %{"notebook_id" => 1}})
       {:ok, :synced}
-
-      iex> perform((%Oban.Job{args: %{"notebook_id" => 2}})
-      {:error, "..."}
 
       iex> perform((%Oban.Job{args: %{"notebook_id" => 3}})
       {:cancel, "..."}
@@ -52,47 +46,47 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
 
   defp get_notebook(notebook_id) do
     with %Notebook{} = notebook <- Notebooks.get_notebook(notebook_id, preload: [:user, :repo]) do
-      %Sync{notebook: notebook}
+      %{notebook: notebook}
     else
-      nil -> %Sync{cancel: "No notebook. Skipping."}
+      nil -> {:cancel, "No notebook. Skipping."}
     end
   end
 
-  defp enqueue_sync_repo_if_no_default_branch(%Sync{} = data) do
+  defp enqueue_sync_repo_if_no_default_branch(data) do
     with %{notebook: %Notebook{repo: %Repo{default_branch: nil} = repo}} <- data do
       {:ok, _job} =
         %{repo_id: repo.id}
         |> RepoSyncWorker.new()
         |> Oban.insert()
 
-      %Sync{cancel: "No default branch. Enqueueing RepoSyncWorker."}
+      {:cancel, "No default branch. Enqueueing RepoSyncWorker."}
     end
   end
 
-  defp get_urls(%Sync{cancel: error} = data) when is_binary(error), do: data
+  defp get_urls({:cancel, error}), do: {:cancel, error}
 
-  defp get_urls(%Sync{} = data) do
+  defp get_urls(data) do
     with {:ok, %Urls{} = urls} <- Urls.get_urls(data.notebook) do
-      %{data | urls: urls}
+      Map.put(data, :urls, urls)
     else
       {:error, error} ->
         Logger.error("get_urls/1 returned {:error, #{error}}; notebook.id=#{data.notebook.id}")
-        %{data | cancel: error}
+        {:cancel, error}
     end
   end
 
-  defp get_content(%Sync{cancel: error} = data) when is_binary(error), do: data
+  defp get_content({:cancel, error}), do: {:cancel, error}
 
   defp get_content(data) do
-    with %Sync{default_branch_content: nil} <- make_default_branch_request(data) do
-      maybe_make_commit_request(data)
-    end
+    data
+    |> make_default_branch_request()
+    |> maybe_make_commit_request()
   end
 
-  defp make_default_branch_request(%Sync{} = data) do
+  defp make_default_branch_request(data) do
     case ReqTools.make_request(data.urls.raw_default_branch_url) do
       {:ok, %Req.Response{status: 200, body: body}} ->
-        %{data | default_branch_content: body}
+        Map.put(data, :default_branch_content, body)
 
       {:ok, %Req.Response{status: 404}} ->
         data
@@ -103,18 +97,21 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
     end
   end
 
-  defp maybe_make_commit_request(%Sync{urls: %Urls{raw_commit_url: nil}} = data), do: data
+  defp maybe_make_commit_request(%{urls: %Urls{raw_commit_url: nil}}),
+    do: {:cancel, "raw_commit_url is nil"}
+
+  defp maybe_make_commit_request(%{default_branch_content: _} = data), do: data
 
   # Make the second request when default_branch_content is nil
   # Then, we'll need to settle for the commit_branch
-  defp maybe_make_commit_request(%Sync{} = data) do
+  defp maybe_make_commit_request(data) do
     case ReqTools.make_request(data.urls.raw_commit_url) do
       {:ok, %Req.Response{status: 200, body: body}} ->
-        %{data | commit_content: body}
+        Map.put(data, :commit_content, body)
 
       {:ok, %Req.Response{status: 404}} ->
         Logger.error("Notebook (id: #{data.notebook.id}) deleted or moved on Github")
-        %{data | cancel: "neither notebook default branch url or commit url exists"}
+        {:cancel, "neither notebook default branch url or commit url exists"}
 
       _ ->
         # Retry job several times
@@ -122,11 +119,10 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
     end
   end
 
-  defp update_content_and_maybe_url(%Sync{cancel: error}) when is_binary(error),
-    do: {:cancel, error}
+  defp update_content_and_maybe_url({:cancel, error}), do: {:cancel, error}
 
   # Save content and url even if they are nil
-  defp update_content_and_maybe_url(%Sync{} = data) do
+  defp update_content_and_maybe_url(data) do
     attrs = attributes_to_update(data)
 
     with {:ok, _notebook} <- Notebooks.update_notebook(data.notebook, attrs) do
@@ -142,14 +138,14 @@ defmodule Notesclub.Workers.UrlContentSyncWorker do
     end
   end
 
-  defp attributes_to_update(%Sync{default_branch_content: nil} = data) do
-    %{content: data.commit_content, url: nil}
+  defp attributes_to_update(%{default_branch_content: default_branch_content, urls: urls}) do
+    %{
+      content: default_branch_content,
+      url: urls.default_branch_url
+    }
   end
 
-  defp attributes_to_update(%Sync{} = data) do
-    %{
-      content: data.default_branch_content,
-      url: data.urls.default_branch_url
-    }
+  defp attributes_to_update(%{commit_content: commit_content}) do
+    %{content: commit_content, url: nil}
   end
 end
