8ba8d9edaa14b5bc8776e4802608dc92a2d3a8cc
Hector Perez <hecpeare@gmail.com>
2023-02-18 19:29:29 +0100

    upgrade elixir 1.14.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Notesclub.MixProject do
     [
       app: :notesclub,
       version: "0.1.0",
-      elixir: "~> 1.13.4",
+      elixir: "~> 1.14.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
