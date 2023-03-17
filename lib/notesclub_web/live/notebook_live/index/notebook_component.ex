c6656a959cac056ba48381fabe0541d09c04bc74
Hector Perez <hecpeare@gmail.com>
2023-03-17 18:11:29 +0100

    refactorize NotebookComponent

--- lib/notesclub_web/live/notebook_live/index/notebook_component.ex
+++ lib/notesclub_web/live/notebook_live/index/notebook_component.ex
@@ -5,6 +5,24 @@ defmodule NotesclubWeb.NotebookLive.Index.NotebookComponent do
   alias Notesclub.Notebooks
   alias Notesclub.Notebooks.Notebook
 
+  defp truncated_title(notebook) do
+    StringTools.truncate(notebook.title, 50)
+  end
+
+  defp truncated_filename(notebook) do
+    StringTools.truncate(notebook.github_filename, 50)
+  end
+
+  defp search_fragment(assigns) do
+    ~H"""
+    <%= if assigns[:search] && !String.contains?(@notebook.github_filename, @search) do %>
+      <p class="text-gray-400">
+        <%= Notebooks.content_fragment(@notebook, @search) %>
+      </p>
+    <% end %>
+    """
+  end
+
   defp format_date(%Notebook{inserted_at: inserted_at}) do
     %NaiveDateTime{year: year, month: month, day: day} = inserted_at
     "#{year}-#{month}-#{day}"
