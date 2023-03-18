35d6de68dc0f076794ff090a38850e861543ac22
Hector Perez <hecpeare@gmail.com>
2023-03-18 18:48:15 +0100

    Add ex_unit to Dialyzer's plt_add_apps

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Notesclub.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      dialyzer: [plt_add_apps: [:mix]]
+      dialyzer: [plt_add_apps: [:mix, :ex_unit]]
     ]
   end
 
