a723a5c0f1b773d2c8224667858e30e558da66bb
Hector Perez <hecpeare@gmail.com>
2023-03-18 11:18:11 +0100

    add two missing @moduledoc

--- lib/notesclub_web/live/notebook_live/index/close_filter_component.ex
+++ lib/notesclub_web/live/notebook_live/index/close_filter_component.ex
@@ -1,3 +1,8 @@
 defmodule NotesclubWeb.NotebookLive.Index.CloseFilterComponent do
+  @moduledoc """
+  Badge that indicates the active filter.
+  Visitors can close it to go back.
+  """
+
   use NotesclubWeb, :live_component
 end
--- lib/notesclub_web/live/notebook_live/index/featured_component.ex
+++ lib/notesclub_web/live/notebook_live/index/featured_component.ex
@@ -1,3 +1,7 @@
 defmodule NotesclubWeb.NotebookLive.Index.FeaturedComponent do
+  @moduledoc """
+  Badges of featured users
+  """
+
   use NotesclubWeb, :live_component
 end
