154f2db4f50a36d8706301f86cd78c1e3a99d7fe
Hector Perez <hecpeare@gmail.com>
2023-03-18 11:42:05 +0100

    Add more notebooks link in show page

--- test/notesclub_web/live/notebook_live/show_test.exs
+++ test/notesclub_web/live/notebook_live/show_test.exs
@@ -6,25 +6,35 @@ defmodule NotesclubWeb.NotebookLive.ShowTest do
   import Notesclub.AccountsFixtures
   import Notesclub.ReposFixtures
 
-  test "GET notebook returns content and user", %{conn: conn} do
+  setup do
     user = user_fixture(%{name: "Hec", username: "hectorperez"})
     repo = repo_fixture(%{name: "myrepo", user_id: user.id})
 
-    file = "hectorperez/myrepo/blob/main/a.livemd"
+    path = "hectorperez/myrepo/blob/main/a.livemd"
 
     notebook_fixture(%{
       content: "# Something\n\netc.",
       title: "Something",
-      url: "https://github.com/#{file}",
+      url: "https://github.com/#{path}",
       user_id: user.id,
       repo_id: repo.id
     })
 
-    {:ok, view, _html} = live(conn, "/#{file}")
+    %{path: path}
+  end
+
+  test "GET notebook returns content and user", %{path: path, conn: conn} do
+    {:ok, view, _html} = live(conn, "/#{path}")
 
     assert render(view) =~ "Something"
     assert render(view) =~ "etc."
     assert render(view) =~ "Hec"
     assert render(view) =~ "@hectorperez"
+  end
+
+  test "GET notebook has More notebooks link", %{path: path, conn: conn} do
+    {:ok, view, _html} = live(conn, "/#{path}")
+
+    assert has_element?(view, "a[href=\"/\"]", "More notebooks")
   end
 end
