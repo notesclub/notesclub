00997dfffe6dcbd6f404f33cf1ca4a7ec9eb1f56
Hector Perez <hecpeare@gmail.com>
2023-03-22 18:14:59 +0100

    Add friendly URLs for notebooks

--- lib/notesclub_web/live/notebook_live/index/notebook_component.ex
+++ lib/notesclub_web/live/notebook_live/index/notebook_component.ex
@@ -7,6 +7,7 @@ defmodule NotesclubWeb.NotebookLive.Index.NotebookComponent do
 
   alias Notesclub.Notebooks
   alias Notesclub.Notebooks.Notebook
+  alias Notesclub.Notebooks.Urls
   alias Notesclub.StringTools
 
   defp truncated_title(notebook) do
@@ -28,7 +29,7 @@ defmodule NotesclubWeb.NotebookLive.Index.NotebookComponent do
   end
 
   defp notebook_path(notebook) do
-    String.replace(notebook.url || notebook.github_html_url, "https://github.com", "")
+    Urls.url_to_path(notebook.url || notebook.github_html_url)
   end
 
   defp format_date(%Notebook{inserted_at: inserted_at}) do
