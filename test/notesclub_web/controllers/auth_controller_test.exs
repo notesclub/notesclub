fe5660db692306df8a0e2025fecd5be4b4e0e178
Fabricio Damazio <fabridamazio@gmail.com>
2024-05-03 00:44:45 -0300

    AuthController tests

---
+++ test/notesclub_web/controllers/auth_controller_test.exs
@@ -0,0 +1,81 @@
+defmodule NotesclubWeb.AuthControllerTest do
+  use NotesclubWeb.ConnCase
+
+  alias NotesclubWeb.AuthController
+  alias Notesclub.Accounts.User
+  alias Notesclub.Accounts
+
+  test "callback/2 persists an error value in flash when fails", %{conn: conn} do
+    auth = %Ueberauth.Failure{}
+
+    conn =
+      conn
+      |> bypass_through(NotesclubWeb.Router, [:browser])
+      |> get("/auth/github/callback")
+      |> assign(:ueberauth_failure, auth)
+      |> AuthController.callback(%{})
+
+    assert Phoenix.Flash.get(conn.assigns.flash, :error) == "Failed to authenticate."
+  end
+
+  test "callback/2 creates a new user and assigns their ID to the session when one does not exist",
+       %{conn: conn} do
+    auth = %Ueberauth.Auth{
+      uid: 123_123,
+      provider: :github,
+      info: %{
+        nickname: "John The Doe",
+        github_id: 123_123,
+        username: "JohnDoe",
+        name: "John Doe",
+        urls: %{
+          avatar_url: "https://example.com/image.jpg"
+        }
+      }
+    }
+
+    assert [] = Accounts.list_users()
+
+    conn =
+      conn
+      |> bypass_through(NotesclubWeb.Router, [:browser])
+      |> get("/auth/github/callback")
+      |> assign(:ueberauth_auth, auth)
+      |> AuthController.callback(%{})
+
+    %{"user_id" => id} = get_session(conn)
+
+    assert %User{id: ^id} = Accounts.get_user(id)
+    assert [%User{}] = Accounts.list_users()
+  end
+
+  test "callback/2 gets the user and assigns their ID to the session when user already exists", %{
+    conn: conn
+  } do
+    params = %{
+      username: "John The Doe",
+      github_id: 123_123,
+      name: "John Doe",
+      avatar_url: "https://example.com/image.jpg"
+    }
+
+    Accounts.create_user(params)
+
+    auth = %Ueberauth.Auth{
+      uid: 123_123,
+      provider: :github
+    }
+
+    conn =
+      conn
+      |> bypass_through(NotesclubWeb.Router, [:browser])
+      |> get("/auth/github/callback")
+      |> assign(:ueberauth_auth, auth)
+      |> AuthController.callback(%{})
+
+    %{"user_id" => id} = get_session(conn)
+
+    assert %User{id: ^id} = Accounts.get_user(id)
+    assert [%User{}] = Accounts.list_users()
+  end
+end
