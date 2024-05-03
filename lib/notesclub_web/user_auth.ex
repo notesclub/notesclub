cfd70a56ff05234f552fe8bc2397dce4e2c1e36d
Fabricio Damazio <fabridamazio@gmail.com>
2024-05-03 11:19:01 -0300

    Include docs in UserAuth plug

--- lib/notesclub_web/user_auth.ex
+++ lib/notesclub_web/user_auth.ex
@@ -1,12 +1,28 @@
 defmodule NotesclubWeb.UserAuth do
+  @moduledoc """
+  A plug for retrieving current user data.
+  """
+
   import Plug.Conn
 
   alias Notesclub.Accounts
 
+  @doc """
+  Handles mounting and retrieving the current_user in LiveViews.
+
+  ## `on_mount` arguments
+
+    * `:mount_current_user` - Assigns current_user
+      to socket assigns based on user_id, or nil if
+      there's no user_id or no matching user.
+  """
   def on_mount(:mount_current_user, _params, session, socket) do
     {:cont, mount_current_user(socket, session)}
   end
 
+  @doc """
+  Retrieves current user information and assigns it to the connection.
+  """
   def fetch_current_user(conn, _opts) do
     user_id = get_session(conn, :user_id)
 
