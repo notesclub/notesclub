077b78c56ec0ccd87c2fe3e7f3046922c23137cb
Henrique Rezende <henrique.64@hotmail.com>
2024-05-11 18:51:33 -0300

    feat: share component

---
+++ lib/notesclub_web/live/notebook_live/share_component.ex
@@ -0,0 +1,28 @@
+defmodule NotesclubWeb.NotebookLive.ShareComponent do
+  @moduledoc """
+  Icon to share to X/Twitter
+  """
+  use Phoenix.Component
+
+  attr :share_to_x_text, :string, required: true
+
+  @doc """
+  Share to X/Twitter icon
+  """
+  def share_to_x(assigns) do
+    ~H"""
+    <.link href={"https://twitter.com/intent/tweet?text=#{@share_to_x_text}"} target="blank_">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
+        <circle cx="256" cy="256" r="256" fill="currentColor"></circle>
+        <g transform="scale(0.5) translate(256 256)">
+          <path
+            d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+            fill="white"
+          >
+          </path>
+        </g>
+      </svg>
+    </.link>
+    """
+  end
+end
