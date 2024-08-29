29c0f3f02013c539e130cfc0f6e55eae5476d6ea
Hector Perez <hecpeare@gmail.com>
2024-08-29 14:08:07 +0200

    Comment old oban migration to avoid warning

--- priv/repo/migrations/20220910110436_add_oban_producers.exs
+++ priv/repo/migrations/20220910110436_add_oban_producers.exs
@@ -2,10 +2,11 @@ defmodule Notesclub.Repo.Migrations.AddObanProducers do
   use Ecto.Migration
 
   def change do
-    if Code.ensure_loaded?(Oban.Pro.Migrations.Producers) do
-      Oban.Pro.Migrations.Producers.change()
-    else
-      :ignored
-    end
+    #  Comment to avoid warning: warning: Oban.Pro.Migrations.Producers.change/0 is undefined (module Oban.Pro.Migrations.Producers is not available or is yet to be defined)
+    # if Code.ensure_loaded?(Oban.Pro.Migrations.Producers) do
+    #   Oban.Pro.Migrations.Producers.change()
+    # else
+    #   :ignored
+    # end
   end
 end
