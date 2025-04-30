51c0bbfa5e3224156ff55c4dbaf64348c6b677ae
Hector Perez <hecpeare@gmail.com>
2025-04-30 22:04:16 +0200

    Don't delete clap_count for now

--- priv/repo/migrations/20250222212602_remove_clap_count_from_notebooks.exs
+++
@@ -1,9 +0,0 @@
-defmodule Notesclub.Repo.Migrations.RemoveClapCountFromNotebooks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:notebooks) do
-      remove :clap_count
-    end
-  end
-end
